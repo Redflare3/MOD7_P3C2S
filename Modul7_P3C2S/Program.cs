a9bae82c45ef83b95246ef335b465be9c182be0d
Sund64 <chris.cornelis.lukito@gmail.com>
2026-04-14 08:19:58 +0700

    Update Program.cs

--- Modul7_P3C2S/Program.cs
+++ Modul7_P3C2S/Program.cs
@@ -7,9 +7,12 @@ class main
     {
         FilmFavorit_103022400030 film1 = new FilmFavorit_103022400030();
         film1.ReadJSON();
-        watc
-    }
-
+        FilmFavorit_103022400075 ff = new FilmFavorit_103022400075();
+        ff.ReadJson();
+        WatchList_103022400075 wl = new WatchList_103022400075();
+        wl.ReadJson();
+        GenreDictionary_103022400075 gd = new GenreDictionary_103022400075();
+        gd.ReadJson();
         Watchlist_103022400141 WL = new Watchlist_103022400141();
         WL.ReadJson();
 
