406733c7392f784a7d8e44c5bbb3f24ab0f19301
unknown <redflare31138@gmail.com>
2026-04-14 07:42:42 +0700

    menambahkan class FilmFavorit_103022400141

--- Modul7_P3C2S/Program.cs
+++ Modul7_P3C2S/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Modul7_P3C2S;
+
+class main
+{
+    public static void Main(string[] args)
+    {
+        FilmFavorit_103022400141 FF = new FilmFavorit_103022400141();
+        FF.ReadJson();
+
+    }
+
+
+}
